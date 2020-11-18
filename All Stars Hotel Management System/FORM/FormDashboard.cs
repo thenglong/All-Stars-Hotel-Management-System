f11419e0f87799b122d007611a861ca304266963
= <=>
2020-11-18 14:05:51 +0700

    Add project files.

---
+++ All Stars Hotel Management System/FORM/FormDashboard.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace All_Stars_Hotel.FORM
+{
+    public partial class FormDashboard : Form
+    {
+        public string Username;
+        public FormDashboard()
+        {
+            InitializeComponent();
+            if (Username is null) Username = "Unknown User";
+        }
+
+        private void MoveSlidePanel(Control btn)
+        {
+            panelSlide.Top = btn.Top;
+            panelSlide.Height = btn.Height;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelDateTime.Text = DateTime.Now.ToString("dd-MMM-yyyy hh:mm:ss tt");
+        }
+
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            labelUserName.Text = Username;
+        }
+
+        private void buttonDashboard_Click(object sender, EventArgs e)
+        {
+            MoveSlidePanel(buttonDashboard);
+        }
+
+        private void buttonClient_Click(object sender, EventArgs e)
+        {
+            MoveSlidePanel(buttonClient);
+        }
+
+        private void buttonRoom_Click(object sender, EventArgs e)
+        {
+            MoveSlidePanel(buttonRoom);
+        }
+
+        private void buttonReservation_Click(object sender, EventArgs e)
+        {
+            MoveSlidePanel(buttonReservation);
+        }
+
+        private void buttonSettings_Click(object sender, EventArgs e)
+        {
+            //MoveSlidePanel(buttonSettings);
+            FormSettings formSettings = new FormSettings();
+            formSettings.Show();
+        }
+
+        private void FormDashboard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure?", "Logout", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.No) e.Cancel = true;
+        }
+
+        private void FormDashboard_KeyDown(object sender, KeyEventArgs e)
+        {
+            // set the Form KeyPreview property to true first then do this
+            if (e.Control && e.KeyCode == Keys.K) buttonSettings.PerformClick();
+        }
+
+        private void labelLogout_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            this.Close();
+        }
+    }
+}
---
+++ All Stars Hotel Management System/FORM/FormLogin.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace All_Stars_Hotel.FORM
+{
+    public partial class FormLogin : Form
+    {
+        private readonly string connString = "server=localhost;user id=test;database=all_stars_hotel;persistsecurityinfo=True;pwd=test";
+
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBoxMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBoxShowPWD_Click(object sender, EventArgs e)
+        {
+            pictureBoxShowPWD.Hide();
+            textBoxPassword.UseSystemPasswordChar = false;
+            pictureBoxHidePWD.Show();
+        }
+
+        private void pictureBoxHidePWD_Click(object sender, EventArgs e)
+        {
+            pictureBoxHidePWD.Hide();
+            textBoxPassword.UseSystemPasswordChar = true;
+            pictureBoxShowPWD.Show();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            var username = textBoxUsername.Text.Trim();
+            var pwd = textBoxPassword.Text.Trim();
+            if (username == String.Empty || pwd == String.Empty)
+            {
+                MessageBox.Show("Please fill out all fields.", "Required field", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            else
+            {
+                var cmdText = $"SELECT username, password FROM user WHERE username = '{username}' and password = '{pwd}'";
+                // MySql Connection
+                MySqlConnection conn = new MySqlConnection(connString);
+                MySqlCommand cmd = new MySqlCommand(cmdText, conn);
+                MySqlDataReader dataReader;
+                try
+                {
+                    // Open connection
+                    conn.Open();
+
+                    // execute find user 
+                    dataReader = cmd.ExecuteReader();
+
+                    // if user exist
+                    if (dataReader.Read())
+                    {
+                        FormDashboard formDashboard = new FormDashboard();
+                        formDashboard.Username = username;
+                        formDashboard.Show();
+                        //textBoxUsername.Clear();
+                        textBoxPassword.Clear();
+                    }
+                    else MessageBox.Show("Invalid Username or Password", "Username or Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void textBoxPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                // set the Form KeyPreview property to true first then do this and say goodbye to the beep sound
+                e.Handled = true;
+                buttonLogin.PerformClick();
+            }
+        }
+    }
+}
